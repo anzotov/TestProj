6612f512d864ce6514fe74341a74fed8e19289a9
Andrey Zotov <Andrey.N.Zotov@yandex.ru>
2024-04-30 11:51:29 +0500

    Identation convert #2

--- Core/Src/tcp_handler.c
+++ Core/Src/tcp_handler.c
@@ -4,43 +4,43 @@
 
 struct tcp_pcb* tcp_create_socket(const u16_t port, tcp_accept_fn accept)
 {
-	// создание сокета
-	struct tcp_pcb* tpcb = tcp_new();
-	// проверяем, что не инициализировали сокет еще
-	if (tpcb == NULL)
-	{
-		return tpcb;
-	}
+    // создание сокета
+    struct tcp_pcb* tpcb = tcp_new();
+    // проверяем, что не инициализировали сокет еще
+    if (tpcb == NULL)
+    {
+        return tpcb;
+    }
 
-	err_t err = tcp_bind(tpcb, IP_ANY_TYPE, port);
-	if (ERR_OK != err)
-	{
-		tcp_close(tpcb);
-		return NULL;
-	}
+    err_t err = tcp_bind(tpcb, IP_ANY_TYPE, port);
+    if (ERR_OK != err)
+    {
+        tcp_close(tpcb);
+        return NULL;
+    }
 
-	struct tcp_pcb* new_tpcb = tcp_listen(tpcb);
-	if (new_tpcb == NULL)
-	{
-		tcp_close(tpcb);
-	}
-	tcp_accept(new_tpcb, accept);
-	return new_tpcb;
+    struct tcp_pcb* new_tpcb = tcp_listen(tpcb);
+    if (new_tpcb == NULL)
+    {
+        tcp_close(tpcb);
+    }
+    tcp_accept(new_tpcb, accept);
+    return new_tpcb;
 }
 
 err_t tcp_send_msg(struct tcp_pcb* tpcb, const char* data)
 {
-	err_t err = ERR_ABRT;
-	// если сокет не создался, то на выход с ошибкой
-	if (tpcb == NULL)
-	{
-		return err;
-	}
-	// отсылаем пакет
-	err = tcp_write(tpcb, data, strlen(data)+1, 0);
-	if (ERR_OK == err)
-	{
-		err = tcp_output(tpcb);
-	}
-	return err;
+    err_t err = ERR_ABRT;
+    // если сокет не создался, то на выход с ошибкой
+    if (tpcb == NULL)
+    {
+        return err;
+    }
+    // отсылаем пакет
+    err = tcp_write(tpcb, data, strlen(data)+1, 0);
+    if (ERR_OK == err)
+    {
+        err = tcp_output(tpcb);
+    }
+    return err;
 }
--- Core/Src/udp_handler.c
+++ Core/Src/udp_handler.c
@@ -4,53 +4,53 @@
 
 struct udp_pcb* udp_create_socket(const u16_t port, udp_recv_fn recv, void *recv_arg)
 {
-	// создание сокета
-	struct udp_pcb* upcb = udp_new();
-	// проверяем, что не инициализировали сокет еще
-	if (upcb == NULL)
-	{
-		return upcb;
-	}
+    // создание сокета
+    struct udp_pcb* upcb = udp_new();
+    // проверяем, что не инициализировали сокет еще
+    if (upcb == NULL)
+    {
+        return upcb;
+    }
 
-	err_t err = udp_bind(upcb, IP_ANY_TYPE, port);
-	if (ERR_OK != err)
-	{
-		udp_remove(upcb);
-		return NULL;
-	}
+    err_t err = udp_bind(upcb, IP_ANY_TYPE, port);
+    if (ERR_OK != err)
+    {
+        udp_remove(upcb);
+        return NULL;
+    }
 
-	// регистрируем колбэк на прием пакета
-	udp_recv(upcb, recv, recv_arg);
-	return upcb;
+    // регистрируем колбэк на прием пакета
+    udp_recv(upcb, recv, recv_arg);
+    return upcb;
 }
 
 err_t udp_send_msg(struct udp_pcb* upcb, const char* data, const ip_addr_t* dst_ip, u16_t dst_port)
 {
-	err_t err = ERR_ABRT;
-	// если сокет не создался, то на выход с ошибкой
-	if (upcb == NULL)
-	{
-		return err;
-	}
-	// аллоцируем память под буфер с данными
-	struct pbuf* p = pbuf_alloc(PBUF_TRANSPORT, strlen(data)+1, PBUF_RAM);
-	if (p == NULL)
-	{
-		return err;
-	}
-	do
-	{
-		// кладём данные в аллоцированный буфер
-		err = pbuf_take(p, data, strlen(data)+1);
-		if (ERR_OK != err)
-		{
-			break;
-		}
+    err_t err = ERR_ABRT;
+    // если сокет не создался, то на выход с ошибкой
+    if (upcb == NULL)
+    {
+        return err;
+    }
+    // аллоцируем память под буфер с данными
+    struct pbuf* p = pbuf_alloc(PBUF_TRANSPORT, strlen(data)+1, PBUF_RAM);
+    if (p == NULL)
+    {
+        return err;
+    }
+    do
+    {
+        // кладём данные в аллоцированный буфер
+        err = pbuf_take(p, data, strlen(data)+1);
+        if (ERR_OK != err)
+        {
+            break;
+        }
 
-		// отсылаем пакет
-		err = udp_sendto(upcb, p, dst_ip, dst_port);
-	} while(0);
-	// очищаем аллоцированную память
-	pbuf_free(p);
-	return err;
+        // отсылаем пакет
+        err = udp_sendto(upcb, p, dst_ip, dst_port);
+    } while(0);
+    // очищаем аллоцированную память
+    pbuf_free(p);
+    return err;
 }
